9da8966bc0acc9b0e4ea447f166e0eeaa52b2dac
kyomel <michaellapandio04@gmail.com>
2025-07-04 21:31:19 +0700

    feat: add timeout and delay endpoints for testing network timeouts

--- server/api/routes.go
+++ server/api/routes.go
@@ -1,8 +1,14 @@
 package api
 
 import (
+	"fmt"
+	"io"
+	"net/http"
+	"time"
+
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/kyomel/go-scalable-educative/network"
 )
 
 func GetRouter() *chi.Mux {
@@ -14,6 +20,24 @@ func GetRouter() *chi.Mux {
 	router.Get("/users", FindUserHandler)
 	router.Post("/users", AddUserHandler)
 	router.Patch("/users", UpdateUserHandler)
+
+	router.Get("/delay", func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(3 * time.Second)
+		w.Write([]byte("Done!\n"))
+	})
+
+	router.Get("/timeout", func(w http.ResponseWriter, r *http.Request) {
+		resp, err := network.NewClient().Get("http://localhost:8080/delay")
+		if err != nil {
+			fmt.Printf("error in API - %v\n", err)
+			w.Write([]byte("error!\n"))
+			w.WriteHeader(http.StatusGatewayTimeout)
+			return
+		}
+
+		resVal, _ := io.ReadAll(resp.Body)
+		w.Write(resVal)
+	})
 
 	return router
 }
